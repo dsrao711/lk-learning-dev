7e02c67a637a90892f147a6457a3fdc48bcc432c
dsrao711 <dsrao0712@gmail.com>
2021-12-21 01:31:02 +0530

    materials done

--- app/api/materials/controllers.js
+++ app/api/materials/controllers.js
@@ -20,6 +20,7 @@ exports.getMaterials = function (req, res) {
 }
 
 //Get materials by Sem
+// Eg : DSA - Paid , 500
 exports.getMaterialsBySem = function (req, res) {
 
     var semester_id = req.body.material_sem_id
@@ -40,6 +41,7 @@ exports.getMaterialsBySem = function (req, res) {
 }
 
 // Get Category by materials
+// Eg : Notes , q paper , formula sheet
 exports.getCategoryByMaterial = function (req, res) {
 
     var material_id = req.body.material_id
@@ -63,8 +65,12 @@ exports.getCategoryByMaterial = function (req, res) {
 }
 
 // Get topics by category 
-// Get all the topics 
+// Get all the topics wheere material = ? and category = ?
+// Eg : Seaching and sorting , arrays , strings 
 exports.getTopics = function (req, res) {
+
+    material_id = req.body.material_id
+    category_id = req.body.category_id
 
     const sql_statement = `
         SELECT 
@@ -72,12 +78,12 @@ exports.getTopics = function (req, res) {
             m.material_name , m.material_id,
             c.category_name , c.category_id
         FROM topic as t
-            JOIN material as m ON t.material_id = m.material_id
-            JOIN category as c ON c.category_id = t.category_id
+            JOIN material as m ON t.material_id = m.material_id and m.material_id = ?
+            JOIN category as c ON c.category_id = t.category_id and c.category_id = ?
     `
 
     try{
-        mySqlConnection.query(sql_statement , (err, rows) => {
+        mySqlConnection.query(sql_statement ,[material_id , category_id], (err, rows) => {
             if (!err) {
                 res.status(200).send(rows)
             } else {
@@ -90,3 +96,50 @@ exports.getTopics = function (req, res) {
     
 }
 
+// Get all pdfs 
+// eg : searching.pdf , sorting.pdf
+exports.getPdfs = function (req, res) {
+
+    topic_id = req.body.topic_id
+
+    const sql_statement = `
+        SELECT * from pdf
+        WHERE topic_id = ?
+    `
+    try{
+        mySqlConnection.query(sql_statement ,[topic_id], (err, rows) => {
+            if (!err) {
+                res.status(200).send(rows)
+            } else {
+                res.status(400).send(err)
+            }
+        })
+    }catch(err){
+        res.status(500).json({"message" : "Internal server error"})
+    }
+    
+}
+
+// Get all videos
+//eg : searching , sorting .....
+exports.getVideos = function (req, res) {
+
+    topic_id = req.body.topic_id
+
+    const sql_statement = `
+        SELECT * from video
+        WHERE topic_id = ?
+    `
+    try{
+        mySqlConnection.query(sql_statement ,[topic_id], (err, rows) => {
+            if (!err) {
+                res.status(200).send(rows)
+            } else {
+                res.status(400).send(err)
+            }
+        })
+    }catch(err){
+        res.status(500).json({"message" : "Internal server error"})
+    }
+    
+}
--- app/api/materials/routes.js
+++ app/api/materials/routes.js
@@ -5,6 +5,8 @@ var controller = require('./controllers')
 router.get('/getMaterials' , controller.getMaterials)
 router.post('/getMaterialsBySem' , controller.getMaterialsBySem)
 router.post('/getCategoryByMaterial' , controller.getCategoryByMaterial)
-router.get('/getTopic' , controller.getTopics)
+router.post('/getTopic' , controller.getTopics)
+router.post('/getPdfs' , controller.getPdfs)
+router.post('/getVideos' , controller.getVideos)
 
 module.exports = router ;
