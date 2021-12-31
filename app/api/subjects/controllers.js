10cabb0b34a33997e69d03800db1f2b86a5d20c7
dsrao711 <dsrao0712@gmail.com>
2021-12-31 12:16:00 +0530

    feat : get subjects by authors

--- app/api/subjects/controllers.js
+++ app/api/subjects/controllers.js
@@ -18,3 +18,27 @@ exports.getSubjects = function(req,res){
         res.status(500).json({"message" : "Internal server error" , "error" : err})
     }
 }
+
+exports.getSubjectsByAuthors = function(req,res){
+
+    var subject_id = req.body.subject_id
+    var sql_statement =  `
+        SELECT 
+            m.author_id , a.author_name
+        FROM material as m
+        JOIN author as a ON m.author_id = a.author_id
+        WHERE subject_id = ?
+    `
+
+    try{
+        mySqlConnection.query(sql_statement, [subject_id] , (err, rows) => {
+            if (!err) {
+                res.status(200).json({data : rows})
+            } else {
+                res.status(400).send(err)
+            }
+        })
+    }catch(err){
+        res.status(500).json({"message" : "Internal server error" , "error" : err})
+    }
+}
--- app/api/subjects/routes.js
+++ app/api/subjects/routes.js
@@ -3,5 +3,6 @@ var controller = require('./controllers')
 
 router.get('/' , controller.getSubjectsPage)
 router.post('/getbySem' , controller.getSubjects)
+router.post('/getByAuthor' , controller.getSubjectsByAuthors)
 
 module.exports = router ;
